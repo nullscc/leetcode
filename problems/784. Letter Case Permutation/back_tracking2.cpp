62f502da6e6b28d1624ed50ef8bbfe3d4920a1d3
nullscc <jarves@foxmail.com>
2021-01-19 15:54:04 +0800

    some comments for problem 784

--- problems/784. Letter Case Permutation/back_tracking2.cpp
+++ problems/784. Letter Case Permutation/back_tracking2.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    // 和back_tracking3.cpp思想是一样的，只不过copy的不一样而已
     vector<string> letterCasePermutation(string S) {
         vector<string> result{S};
         for (int i = 0; i < S.size(); i++) {
@@ -8,7 +9,7 @@ public:
                 for (int j = 0; j < size; j++) {
                     string temp = result[j];
                     temp[i] = tolower(temp[i]);
-                    result.emplace_back(move(temp));
+                    result.emplace_back(move(temp));    // 这里move的作用在于避免内存的搬迁或者内存拷贝
                 }
             } else if (islower(S[i])) {
                 int size = result.size();
